8e4bfcca5a5bb66f80414c4d43dea58586e09b8a
SuperGent <paul@gentmail.com>
2014-09-29 13:29:17 +0100

    Initial Tests

---
+++ BowlingScoreKeeper/Player.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingScoreKeeper
+{
+    public class Player
+    {
+        private int[] _balls = new int [21];
+        private int _currentBall = 0;
+
+        public int Score()
+        {
+            int score = 0;
+            for (int i = 0; i < _balls.Length; i++)
+            {
+                score += _balls[i];
+            }
+            return score;
+        }
+
+        public void Ball(int pins)
+        {
+            _balls[_currentBall] = pins;
+            _currentBall++;
+        }
+    }
+}
---
+++ BowlingScoreKeeperTests/UnitTest1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingScoreKeeper;
+
+namespace BowlingScoreKeeperTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+        [TestMethod]
+        public void CheckScoreStartsZero()
+        {
+            var player = new Player();
+
+            var result = player.Score();
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void ZeroScoreGame()
+        {
+            var player = new Player();
+            for (int i = 1; i < 21; i++)
+            {
+                player.Ball(0);
+            }
+
+            var result = player.Score();
+
+            Assert.AreEqual(0, result);
+        }
+        [TestMethod]
+        public void AllOneScoreGame()
+        {
+            var player = new Player();
+            for (int i = 1; i < 21; i++)
+            {
+                player.Ball(1);
+            }
+
+            var result = player.Score();
+
+            Assert.AreEqual(20, result);
+        }
+        [TestMethod]
+        public void NoSpareOrStrikes()
+        {
+            var player = new Player();
+            var testScore = new int[] { 9, 0, 3, 5, 6, 1, 3, 6, 8, 1, 5, 3, 2, 5, 8, 0, 7, 1, 8, 1 };
+            
+            for (int i = 0; i < 20; i++)
+            {
+                player.Ball(testScore[i]);
+            }
+
+            var result = player.Score();
+
+            Assert.AreEqual(82, result);
+        }
+        [TestMethod]
+        public void GameWithSpares()
+        {
+            var player = new Player();
+            var testScore = new int[] { 9,0,3,7,6,1,3,7,8,1,5,5,0,10,8,0,7,3,8,2,8 };
+
+            for (int i = 0; i < 21; i++)
+            {
+                player.Ball(testScore[i]);
+            }
+
+            var result = player.Score();
+
+            Assert.AreEqual(131,result);
+        }
+
+    }
+}
